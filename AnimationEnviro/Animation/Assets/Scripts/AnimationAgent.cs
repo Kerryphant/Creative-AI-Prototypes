14c60e0cc0ffb0a14bbd6d9e329eb474f653974a
Calum Laverock <c.r.laverock@gmail.com>
2020-04-19 01:38:48 +0100

    Added models

---
+++ AnimationEnviro/Animation/Assets/Scripts/AnimationAgent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using MLAgents;
+using MLAgentsExamples;
+
+public class AnimationAgent : Agent
+{
+    public Vector3 startPos;
+
+    public override void InitializeAgent()
+    {
+        startPos = transform.position;
+    }
+
+    public override void CollectObservations()
+    {
+
+    }
+
+    public override void AgentAction(float[] vectorAction)
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public override void AgentReset()
+    {
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        transform.position = startPos;
+    }
+}
---
+++ AnimationEnviro/Animation/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+    }
+
+    void onTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "walker")
+        {
+            //collider.Respawn();
+        }
+    }
+}
