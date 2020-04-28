2265e40fdccef29a62d6bc791e8d7ddfe5b70016
shamungie <shamungie@gmail.com>
2020-04-28 12:45:11 +0100

    Wwise sound bank
    Didnt generate the sound back last time should work now

---
+++ TankEnviro/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID FIRE = 2678880713U;
+        static const AkUniqueID HIT_COLLISION = 653829709U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID TREADS = 2707083042U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace TANK_COLLISION
+        {
+            static const AkUniqueID GROUP = 3215442724U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID TANK = 3206747537U;
+                static const AkUniqueID WALL = 2108779961U;
+            } // namespace SWITCH
+        } // namespace TANK_COLLISION
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
